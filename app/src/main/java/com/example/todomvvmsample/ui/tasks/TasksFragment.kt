4cd62e87b1af18b3963f4b182e0d1036d1b19c5d
Alexey Nimas <mail@alexnimas.ru>
2021-02-22 19:00:09 +0300

    Started implementation navigation

---
+++ app/src/main/java/com/example/todomvvmsample/ui/tasks/TasksFragment.kt
@@ -0,0 +1,7 @@
+package com.example.todomvvmsample.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.example.todomvvmsample.R
+
+class TasksFragment : Fragment(R.layout.fragment_tasks) {
+}
